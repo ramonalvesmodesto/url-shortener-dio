0ffd7fc92bdc4c18180c50dca4a45ec3985f506b
ramonalvesmodesto <ramonmodesto16@protonmail.com>
2022-05-29 14:48:13 -0300

    Primeiro commit

---
+++ src/config/Constants.ts
@@ -0,0 +1,4 @@
+export const config = {
+    API_URL: 'http://localhost:5000',
+    MONGO_CONNECTION: 'mongodb+srv://Ramon:fk3WgPSMGsKnlVf8@url-shortener-dio.crymm.mongodb.net/?retryWrites=true&w=majority',
+}
---
+++ src/controller/URLController.ts
@@ -0,0 +1,33 @@
+import { config } from "../config/Constants";
+import { Request, Response } from "express";
+import shortId from 'shortid'
+import { URLModel } from "../database/model/URL";
+
+export class URLController {
+    public async shorten(req: Request, res: Response): Promise<void> {
+        const {originURL} = req.body;
+        const url = await URLModel.findOne({originURL})
+
+        if(url) {
+            res.json(url)
+            return
+        }
+
+        const hash = shortId.generate();
+        const shortURL = `${config.API_URL}/${hash}`;
+        const newUrl= await URLModel.create({hash, shortURL, originURL});
+        res.json(newUrl);
+    }
+
+    public async redirect(req: Request, res: Response): Promise<void> {
+        const { hash } = req.params;
+        const url = await URLModel.findOne({hash});
+
+        if(url) {
+            res.redirect(url.originURL);
+            return
+        }
+
+        res.status(400).json({error: 'URL not found'});
+    }
+}
